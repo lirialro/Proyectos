9569eb013c4cbe91405244c56d320d868008fa50
lirialro <lirialro@gmail.com>
2020-10-21 11:13:44 +0200

    añadido

---
+++ vectorgit.ts
@@ -0,0 +1,61 @@
+class Vector 
+{
+    private elements = new Array();
+  
+
+    constructor (n: number, k: number) {
+            for (let i=0; i<n; i++)
+            {
+                let aleatorio = Math.round(Math.random() *k);  
+                this.elements.push(aleatorio);
+            }
+    }
+
+    public print ()
+    { 
+        console.log("Elements = " + this.elements)
+    }
+
+    public add (v1: Vector) 
+    {
+        let sumavectores = new Array();
+        for (let i=0; i<this.elements.length; i++)
+        {
+           let suma = this.elements[i] + v1.elements[i];
+           sumavectores.push(suma);
+        }
+        console.log("Suma = " + sumavectores);
+    }
+
+    public subs (v1: Vector) 
+    {
+        let restavectores = new Array();
+        for (let i=0; i<this.elements.length; i++)
+        {
+           let resta = this.elements[i] - v1.elements[i];
+
+           restavectores.push(resta);
+        }
+        console.log("Resta = " +restavectores)
+    }
+
+    public mult (v1: Vector)
+    {
+        let multiplicavectores = new Array();
+        for (let i=0; i<this.elements.length; i++)
+        {
+           let producto = this.elements[i] * v1.elements[i];
+
+           multiplicavectores.push(producto);
+        }
+        console.log("Producto = " + multiplicavectores)
+    }
+
+    
+
+
+
+
+}
+
+module.exports = {Vector}
--- vectorgittest.ts
+++ vectorgittest.ts
@@ -6,7 +6,11 @@ va = new Vector (10,20);
 v1 = new Vector (10, 20);
 
 
+<<<<<<< HEAD
 va.print();
+=======
+
+>>>>>>> 787afa54a47902ad0b2b2252ce9368d515de220f
 va.add(v1); 
 va.subs(v1);
 va.mult(v1); 
