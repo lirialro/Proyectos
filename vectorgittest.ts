8f4c7c1aa331a7830716e8e461517eb8bda0cc0c
lirialro <lirialro@gmail.com>
2020-10-21 13:19:51 +0200

    listo

--- vectorgittest.ts
+++ vectorgittest.ts
@@ -5,17 +5,7 @@ var myLib = require ("./Vector");
 va = new Vector (10,20);
 v1 = new Vector (10, 20);
 
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 va.print();
-=======
-
->>>>>>> 787afa54a47902ad0b2b2252ce9368d515de220f
-=======
-
->>>>>>> 787afa54a47902ad0b2b2252ce9368d515de220f
 va.add(v1); 
 va.subs(v1);
 va.mult(v1); 
-va.multNumber(5); 
